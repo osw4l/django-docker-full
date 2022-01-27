4a7813f7ca69b9c36762033fb18c317b4e19a464
osw4l <ioswxd@gmail.com>
2022-01-27 13:32:03 -0500

    fixed and readme updated

--- apps/main/models.py
+++ apps/main/models.py
@@ -108,7 +108,7 @@ class Sms(models.Model):
     )
     phone = models.CharField(max_length=15)
     sms = models.TextField()
-    success = models.NullBooleanField()
+    success = models.BooleanField(null=True)
     source = models.CharField(
         max_length=20
     )
